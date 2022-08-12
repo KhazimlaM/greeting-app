16837c58c8c004e23df8b3dcf52b320b19e28524
Khazimla <20113497@rcconnect.co.za>
2022-08-12 14:14:38 +0200

    css changes

--- index.js
+++ index.js
@@ -12,13 +12,28 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
 app.get("/", function(req, res){
-    res.send("My Greetings Applictation");
+    res.render("index")
   });
+
+  app.post("/greet", function(req, res){
+    res.redirect("index")
+  });
+
+  app.get("/", function(req, res){
+    res.render("index")
+   
+  });
+
+  app.post("/greet", function(req, res){
+    res.redirect("index")
+  });
+
 
 
 
